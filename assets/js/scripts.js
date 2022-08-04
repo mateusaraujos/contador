246374836dcebd179de986c3573a7c56ea1cda42
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2022-08-04 01:41:04 -0300

    incremento de número

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,13 @@
 // Pegando o número que será decrementado e incrementado.
-let numberSpan = document.getElementById('currentNumber');
+let numberSpan = document.getElementById("currentNumber");
 let currentNumber = 0;
 
+// Pegando os botões que vão decrementar e incrementar.
+let addBtn = document.getElementById("add");
+let subtractBtn = document.getElementById("subtract");
+
+// Incrementando um número.
+addBtn.addEventListener("click", () => {
+    currentNumber += 1;
+    numberSpan.innerHTML = currentNumber;
+})
